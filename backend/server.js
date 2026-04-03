64626741ac7efe35cd9d2462f1b8e7cdecf5d75d
kelechiemmanuel <ekelechi413@gmail.com>
2026-04-03 19:47:10 +0100

    done

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,18 @@ app.get('/', async  (req, res) => {
     }
 })
 
+app.post('/', async (req, res) => {
+    const {title, description} = req.body;
+    try {
+        const result = await pool.query("INSERT INTO posts (title, description) VALUEs ($1, $2) RETURNING *", [title, description]);
+        res.json(result.rows[0])
+    } catch (error) {
+        console.log(error);
+        
+    }
+
+})
+
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
